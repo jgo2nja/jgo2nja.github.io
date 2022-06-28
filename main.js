296b1da15c3b1f56b9edaa5058fa3ef9bbd0a8aa
James <108314373+jgo2nja@users.noreply.github.com>
2022-06-28 17:46:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-function drop() {
-  document.getElementById("menu").classList.toggle("show");
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(thing){
-  if(!thing.target.matches('.pushDrop'){
-     var options = document.getElementsByClassName("dropOptions");
-     var iter;
-     for (iter=0; iter < options.length; iter++){
-       var openDrop = options[i];
-       if(openDrop.classList.contains('show')){
-         openDrop.classList.remove('show');
-       }
-     }
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
 }
